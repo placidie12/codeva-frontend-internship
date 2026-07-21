8e49b60da2f45d3e9d3400686bbfa8fe79ecd6db
Placidie12 <mwizerwaplacidie@gmail.com>
2026-07-21 11:53:53 +0200

    Update frontend project

--- Level 1/Task 1 Responsive Landing Page/script.js
+++ Level 1/Task 1 Responsive Landing Page/script.js
@@ -2,4 +2,8 @@ const menuBtn = document.querySelector(".menu-btn");
 const navLinks = document.querySelector(".nav-links");
 menuBtn.addEventListener("click",()=>{
     navLinks.classList.toggle("active");
+    navLinks.computedStyleMap.display =
+    navLinks.computedStyleMap.display == "flex"
+    ? "none"
+    : "flex";
 });
